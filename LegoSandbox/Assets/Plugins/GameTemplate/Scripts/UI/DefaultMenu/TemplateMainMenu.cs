ef8b555ef1711a2e2163a23a82da403b511b711a
Team-on <timonsol13@gmail.com>
2021-02-08 15:16:24 +0200

    Scene prefabs

--- LegoSandbox/Assets/Plugins/GameTemplate/Scripts/UI/DefaultMenu/TemplateMainMenu.cs
+++ LegoSandbox/Assets/Plugins/GameTemplate/Scripts/UI/DefaultMenu/TemplateMainMenu.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class TemplateMainMenu : MenuBase {
 	[SerializeField] int sceneIdToLoad = 1;
+	[SerializeField] int testScene1 = 2;
+	[SerializeField] int testScene2 = 3;
 
 	public void Play() {
 		SceneLoader.Instance.LoadScene(sceneIdToLoad, true, true);
-
 	}
 
 	public void Load() {
 		SceneLoader.Instance.LoadScene(sceneIdToLoad, false, false);
+	}
+
+	public void PlayTestScene1() {
+		SceneLoader.Instance.LoadScene(testScene1, true, true);
+	}
+
+	public void PlayTestScene2() {
+		SceneLoader.Instance.LoadScene(testScene2, true, true);
 	}
 }
