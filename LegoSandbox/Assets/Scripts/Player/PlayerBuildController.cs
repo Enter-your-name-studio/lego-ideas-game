26823bffd71b456da2ea98c804f436adf7774cd1
Team-on <timonsol13@gmail.com>
2021-02-09 03:41:22 +0200

    Simple build

--- LegoSandbox/Assets/Scripts/Player/PlayerBuildController.cs
+++ LegoSandbox/Assets/Scripts/Player/PlayerBuildController.cs
@@ -1,11 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBuildController : MonoBehaviour {
+	[Header("Refs")]
+	[SerializeField] GameObject[] buildPrefabs;
+	[SerializeField] int currPrefab = 0;
 
+	bool isHit;
+	Ray ray;
+	RaycastHit hit;
+	int layer;
 
 	private void Awake() {
+		layer = LayerMask.GetMask("Brick");
+
 		GameManager.Instance.input.onBuild += Build;
 		GameManager.Instance.input.onBroke += Broke;
 	}
@@ -16,10 +23,28 @@ public class PlayerBuildController : MonoBehaviour {
 	}
 
 	void Build() {
-		Debug.Log("Build");
+		DoRay();
+		if (!isHit)
+			return;
+
+		GameObject prefab = buildPrefabs[currPrefab];
+		currPrefab = (int)Mathf.Repeat(currPrefab + 1, buildPrefabs.Length);
+
+		GameObject spawned = Instantiate(prefab, hit.point, Quaternion.identity, null);
+		spawned.transform.position = hit.point;
 	}
 
 	void Broke() {
-		Debug.Log("Broke");
+		DoRay();
+		if (!isHit)
+			return;
+
+	}
+
+	void DoRay() {
+		ray = TemplateGameManager.Instance.Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+		isHit = Physics.Raycast(ray, out hit, 100, layer, QueryTriggerInteraction.Ignore);
+
+		Debug.Log($"Ray - {isHit} {hit.point}");
 	}
 }
